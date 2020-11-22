a1c8d419c4d831bcd2e759d795e5ae5aeea42c5b
FranciscoMendes10866 <avizinhadochico@gmail.com>
2020-11-22 02:51:51 +0100

    added axios, redux, react-router-dom and react suite

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import { FlexboxGrid, Button } from 'rsuite';
+
+const App = () => {
+  return (
+    <FlexboxGrid justify="center">
+      <FlexboxGrid.Item>
+        <h3>React + React Suite UI + Redux = 🌈 🤯 🧿</h3>
+        <Button appearance="primary">Magic 🎩</Button>
+      </FlexboxGrid.Item>
+    </FlexboxGrid>
+  )
+}
+
+export default App
