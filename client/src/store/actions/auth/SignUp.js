869a3bed4c5c9f37576699796c329ab3a0d75572
FranciscoMendes10866 <avizinhadochico@gmail.com>
2020-11-24 23:12:12 +0100

    signup works 🎉

--- client/src/store/actions/auth/SignUp.js
+++ client/src/store/actions/auth/SignUp.js
@@ -1,5 +1,19 @@
+import axios from 'axios'
+
 const SignUp = ({ username, email, password }) => (dispatch) => {
-    console.log({ username, email, password })
+    const formData = { username, email, password }
+    const baseURL = 'http://localhost:3275/api/v1/auth/sign_up'
+    axios.post(baseURL, formData)
+        .then(({ data }) => {
+            console.log(data)
+            dispatch({
+                type: 'SIGN_UP',
+                payload: data,
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
 }
 
 export default SignUp
