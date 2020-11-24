c95f7264bfd796e5a4663bf1db354771fc39703a
FranciscoMendes10866 <avizinhadochico@gmail.com>
2020-11-24 23:26:56 +0100

    dashboard useSelector 👺

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,11 @@
+import { useSelector } from 'react-redux'
+
 const Dashboard = () => {
+    const username = useSelector((state) => state.auth.username)
     return (
         <div>
             <h1>Dashboard</h1>
+            <h3>🎉 { username } 🎉</h3>
         </div>
     )
 }
