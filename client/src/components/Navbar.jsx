888d33f8a9ade76d7b28ce1f92737bc35a8d977a
FranciscoMendes10866 <avizinhadochico@gmail.com>
2020-11-25 01:25:06 +0100

    navbar with conditional rendering 🎩

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { Link } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { DiIntellij } from 'react-icons/di'
 import {
   Collapse,
   Navbar,
@@ -10,47 +13,115 @@ import {
   UncontrolledDropdown,
   DropdownToggle,
   DropdownMenu,
-  DropdownItem,
-  NavbarText
+  DropdownItem
 } from 'reactstrap';
 
 const NavbarComponent = () => {
+  const linkStyles = {
+    textDecoration: 'none',
+    color: 'black',
+  }
   const [isOpen, setIsOpen] = useState(false);
-
   const toggle = () => setIsOpen(!isOpen);
-
+  const stateToken = useSelector((state) => state.auth.token)
+  let isLoggedIn
+  if (stateToken === null) {
+    isLoggedIn = (
+      <>
+        <NavItem>
+          <NavLink>
+            <Link
+              style={{
+                textDecoration: linkStyles.textDecoration,
+                color: linkStyles.color
+              }}
+              to="/"
+            >
+              Sign up
+                    </Link>
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink>
+            <Link
+              style={{
+                textDecoration: linkStyles.textDecoration,
+                color: linkStyles.color
+              }}
+              to="/sign-in"
+            >
+              Sign in
+                    </Link>
+          </NavLink>
+        </NavItem>
+      </>
+    )
+  } else {
+    isLoggedIn = (
+      <>
+        <NavItem>
+          <NavLink>
+            <Link
+              style={{
+                textDecoration: linkStyles.textDecoration,
+                color: linkStyles.color
+              }}
+              to="/dashboard"
+            >
+              Contacts
+                    </Link>
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink>
+            <Link
+              style={{
+                textDecoration: linkStyles.textDecoration,
+                color: linkStyles.color
+              }}
+              to="/create"
+            >
+              Add Contact
+                    </Link>
+          </NavLink>
+        </NavItem>
+        <UncontrolledDropdown nav inNavbar>
+          <DropdownToggle nav caret>
+            <span
+              style={{
+                textDecoration: linkStyles.textDecoration,
+                color: linkStyles.color
+              }}
+            >Account</span>
+          </DropdownToggle>
+          <DropdownMenu right>
+            <DropdownItem>
+              Log out
+                </DropdownItem>
+          </DropdownMenu>
+        </UncontrolledDropdown>
+      </>
+    )
+  }
   return (
     <div>
       <Navbar color="light" light expand="md">
-        <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <NavbarBrand>
+          <Link
+            style={{
+              textDecoration: linkStyles.textDecoration,
+              color: linkStyles.color
+            }}
+            to="/"
+          >
+            <DiIntellij />
+          </Link>
+        </NavbarBrand>
         <NavbarToggler onClick={toggle} />
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
-            <NavItem>
-              <NavLink href="/components/">Components</NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
-            </NavItem>
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
-                Options
-              </DropdownToggle>
-              <DropdownMenu right>
-                <DropdownItem>
-                  Option 1
-                </DropdownItem>
-                <DropdownItem>
-                  Option 2
-                </DropdownItem>
-                <DropdownItem divider />
-                <DropdownItem>
-                  Reset
-                </DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
+            {isLoggedIn}
           </Nav>
-          <NavbarText>Simple Text</NavbarText>
         </Collapse>
       </Navbar>
     </div>
